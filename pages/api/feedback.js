a8778fc57ec612874af2dd763c83164eb1618220
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-09-12 00:04:16 +0530

    Added : backend api route

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,15 +1,40 @@
 // from here we don't export a react component,
 // instead we create a function to handle incoming request,
 // this function receives two arguments : request object and response object
+import fs from "fs";
+import path from "path";
 
 function handler(req, res) {
   // here we can execute any server side code
   // any code we write here will never end up in client side code bundling, just as code written in
   // getServerSideProps.
 
-  res.status(200).json({
-    message: "This Works",
-  });
+  if (req.method == "POST") {
+    const email = req.body.email;
+    const text = req.body.text;
+
+    const newFeedbackObj = {
+      id: new Date().toISOString(),
+      email: email,
+      text: text,
+    };
+
+    // Store in database or in a file
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+
+    const data = JSON.parse(fileData);
+
+    data.push(newFeedbackObj);
+
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+    res.status(201).json({ message: "Success", feedback: newFeedbackObj });
+  } else {
+    res.status(200).json({
+      message: "This Works",
+    });
+  }
 }
 
 export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,47 @@
-import Head from "next/head";
-import Image from "next/image";
+import React, { useRef } from "react";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(e) {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const requestBody = {
+      email: enteredEmail,
+      text: enteredFeedback,
+    };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <>
       <h1>Home Page</h1>
-      <form></form>
+      <form>
+        <div>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button onClick={submitFormHandler}>Send Feedback</button>
+      </form>
     </>
   );
 }
